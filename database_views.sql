1aa3bd431c9416421c35e88861da0d6fb412595c
weismanm12 <112783326+weismanm12@users.noreply.github.com>
2023-06-14 17:00:38 -0400

    Update database_views.sql
    Alter views to reflect category table changes

--- database_views.sql
+++ database_views.sql
@@ -1,3 +1,4 @@
+-- No update needed
 CREATE VIEW account_balances AS
 SELECT
 	tf.account_id,
@@ -8,23 +9,24 @@ FROM transaction_facts AS tf
 JOIN account USING (account_id)
 GROUP BY account_id, account_type;
 
-CREATE VIEW running_amount_spent AS 
+-- Updated to match 'category' changes
+CREATE VIEW running_amount_spent AS
 SELECT 
 	tf.short_date,
     t.transaction_type_description,
     c.category_description,
     tf.transaction_description,
-    ABS(tf.transaction_amount) AS transaction_total,
     d.year,
+    ABS(tf.transaction_amount) AS transaction_total,
 	SUM(ABS(tf.transaction_amount)) OVER(PARTITION BY d.year ORDER BY short_date ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS running_yearly_spend
 FROM transaction_facts AS tf
 JOIN transaction_type AS t USING (transaction_type_id)
 JOIN category AS c USING (category_id)
 JOIN date AS d USING (short_date)
-WHERE t.transaction_type_description IN ('credit card purchase', 'debit card purchase', 'withdraw')
-	AND year != '2022'
+WHERE year != '2022'
 ORDER BY short_date;
 
+-- Updated to reflect 'category' changes
 CREATE VIEW monthly_spend_summary AS
 SELECT
 	d.month_number,
@@ -38,11 +40,11 @@ FROM transaction_facts AS tf
 JOIN transaction_type AS t USING (transaction_type_id)
 JOIN category AS c USING (category_id)
 JOIN date AS d USING (short_date)
-WHERE t.transaction_type_description IN ('credit card purchase', 'debit card purchase', 'withdraw')
-	AND d.year != '2022'
+WHERE d.year != '2022'
 GROUP BY month, d.month_number, d.year
 ORDER BY d.year, d.month_number;
 
+-- Updated to match 'category' changes
 CREATE VIEW month_spend_category AS
 SELECT
     d.year,
@@ -56,12 +58,12 @@ JOIN category AS c USING (category_id)
 JOIN account AS a USING (account_id)
 JOIN transaction_type AS t USING (transaction_type_id)
 JOIN date AS d USING (short_date)
-WHERE t.transaction_type_description IN ('credit card purchase', 'debit card purchase', 'withdraw')
-	AND d.year != '2022'
+WHERE d.year != '2022'
 GROUP BY month_number, month_name, year, category_description
 ORDER BY year, month_number, month_ranking;
 
-CREATE VIEW daily_spend_sum AS
+-- Updated to match changes in 'category' table
+ALTER VIEW daily_spend_sum AS
 SELECT d.short_date, SUM(COALESCE(SUM(ABS(tf.transaction_amount)), 0)) OVER(ORDER BY d.short_date ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS cumulative_sum
 FROM date AS d
 LEFT JOIN transaction_facts AS tf
@@ -70,11 +72,12 @@ LEFT JOIN transaction_type AS t
 	ON tf.transaction_type_id = t.transaction_type_id
 WHERE EXTRACT(year FROM d.short_date) != '2022'
 	AND d.short_date <= CURDATE()
-	AND (t.transaction_type_description IN ('credit card purchase', 'debit card purchase', 'withdraw') OR t.transaction_type_description IS NULL)
+	AND (t.transaction_type_id IN (1, 2) OR t.transaction_type_id IS NULL)
 GROUP BY d.short_date
 ORDER BY short_date;
 
-CREATE VIEW daily_spend_sum_w_transactions AS 
+-- Updated to match changes in 'category' table
+CREATE VIEW daily_spend_sum_w_transactions AS
 SELECT 
 	d.short_date,
     t.transaction_type_description,
@@ -89,6 +92,38 @@ LEFT JOIN transaction_facts AS tf
 LEFT JOIN transaction_type AS t USING (transaction_type_id)
 LEFT JOIN category AS c USING (category_id)
 WHERE d.short_date <= CURDATE()
-	AND (t.transaction_type_description IN ('credit card purchase', 'debit card purchase', 'withdraw') OR t.transaction_type_description IS NULL)
+	AND (t.transaction_type_id IN (1, 2) OR t.transaction_type_id IS NULL)
 	AND year != '2022'
 ORDER BY short_date;
+
+-- Updated to match changes in 'category' table
+CREATE VIEW daily_category_balance AS
+WITH date_category AS (
+SELECT 
+	d.short_date,
+    c.category_id
+FROM date AS d
+CROSS JOIN (
+	SELECT category_id
+    FROM category
+) AS c
+WHERE EXTRACT(year FROM d.short_date) != '2022'
+	AND d.short_date <= CURDATE()
+ORDER BY short_date, category_id
+)
+
+SELECT 
+	dc.short_date,
+    dc.category_id,
+    SUM(COALESCE(SUM(ABS(tf.transaction_amount)), 0)) OVER(PARTITION BY dc.category_id ORDER BY dc.short_date ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS cumulative_sum
+FROM date_category AS dc
+LEFT JOIN transaction_facts AS tf
+	ON dc.short_date = tf.short_date
+    AND dc.category_id = tf.category_id
+LEFT JOIN transaction_type AS t
+	ON tf.transaction_type_id = t.transaction_type_id
+WHERE EXTRACT(year FROM dc.short_date) != '2022'
+	AND dc.short_date <= CURDATE()
+	AND (t.transaction_type_id IN (1, 2) OR t.transaction_type_id IS NULL)
+GROUP BY dc.short_date, dc.category_id
+ORDER BY dc.short_date, dc.category_id
