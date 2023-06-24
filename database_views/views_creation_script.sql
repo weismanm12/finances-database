a9e2fc030d82e02ee2a81ba7d3943dbf74c3f171
Matthew Weisman <112783326+weismanm12@users.noreply.github.com>
2023-06-23 22:39:39 -0400

    Update views_creation_script.sql

--- database_views/views_creation_script.sql
+++ database_views/views_creation_script.sql
@@ -1,50 +1,45 @@
 -- Shows total spent per month, increase from prior month, and the running total for the year the month is in
 CREATE VIEW monthly_spend_summary AS
+WITH cte AS (											-- CTE to perform initial aggregation
+	SELECT
+		d.month_number,
+		d.month_name AS month,
+		d.year,
+		SUM(ABS(tf.transaction_amount)) AS month_amount_spent				-- Take absolute value since transaction_amount is negative for purchases
+	FROM transaction_facts AS tf
+	JOIN transaction_type AS t 
+		USING (transaction_type_id)
+	JOIN category AS c 
+		USING (category_id)
+	JOIN date AS d 
+		USING (short_date)
+	WHERE d.year != '2022'			-- Not interested in 2022
+	GROUP BY month, d.month_number, d.year
+	ORDER BY d.year, d.month_number
+)
 SELECT
-	d.month_number,
-	d.month_name AS month,
-    d.year,
-	SUM(ABS(tf.transaction_amount)) AS month_amount_spend,						-- Take absolute value since transaction_amount is negative for purchases
-	ROUND(																		-- Get percent increase from prior month
-		 (
-		 SUM(ABS(tf.transaction_amount)) - LAG(SUM(ABS(tf.transaction_amount)))
-				OVER(ORDER BY d.year, d.month_number)
-		 )
-		 / NULLIF(
-				  LAG(SUM(ABS(tf.transaction_amount))) 
-				      OVER(ORDER BY d.year, d.month_number),
-                  0
-			),
-		 2
-	) AS prior_month_increase,
-    SUM(SUM(ABS(tf.transaction_amount)))
-		OVER(
-			PARTITION BY year 
-			ORDER BY d.year, d.month_number
-			) AS yearly_running_total
-FROM transaction_facts AS tf
-JOIN transaction_type AS t 
-	USING (transaction_type_id)
-JOIN category AS c 
-	USING (category_id)
-JOIN date AS d 
-	USING (short_date)
-WHERE d.year != '2022'
-GROUP BY month, d.month_number, d.year
-ORDER BY d.year, d.month_number;
--- ------------------------------------------------------------------------------------------------------------------------------------------------------
-# Shows and ranks amount spent per category per month
+	*,
+    	ROUND((month_amount_spent - LAG(month_amount_spent) OVER (ORDER BY year, month_number))				-- Calculate dollar increase from prior month
+		/ LAG(month_amount_spent) OVER (ORDER BY year, month_number), 2) AS prior_month_change,			-- Convert to decimal value of last month
+	SUM(month_amount_spent)												-- Calculate yearly running total spent
+		OVER (
+			PARTITION BY year
+			ORDER BY year, month_number
+        ) AS yearly_running_total
+FROM cte;
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Shows and ranks amount spent per category per month
 CREATE VIEW monthly_spend_category AS
 SELECT
-    d.year,
-    d.month_number,
-    d.month_name,
-    c.category_description,
-    SUM(ABS(tf.transaction_amount)) AS monthly_spend,								-- Take absolute value since transaction_amount is negative for purchases
-    RANK() 
+	d.year,
+    	d.month_number,
+    	d.month_name,
+    	c.category_description,
+    	SUM(ABS(tf.transaction_amount)) AS monthly_spend,								-- Take absolute value since transaction_amount is negative for purchases
+    	RANK() 
 		OVER(
 			PARTITION BY month_number, month_name, year 
-            ORDER BY ABS(SUM(tf.transaction_amount)) DESC
+            		ORDER BY ABS(SUM(tf.transaction_amount)) DESC
 		) AS month_ranking
 FROM transaction_facts AS tf
 JOIN category AS c 
@@ -58,34 +53,36 @@ JOIN date AS d
 WHERE d.year != '2022'
 GROUP BY month_number, month_name, year, category_description
 ORDER BY year, month_number, month_ranking;
--- ------------------------------------------------------------------------------------------------------------------------------------------------------
-# Shows all transactions and the yearly running total spent
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Shows all transactions and the yearly running total spent
 CREATE VIEW daily_spend AS
-SELECT 
+SELECT
+	tf.transaction_id,
 	d.short_date,
-    t.transaction_type_description,
-    c.category_description,
-    tf.transaction_description,
-    COALESCE(ABS(tf.transaction_amount), 0) AS transaction_total,		-- Take absolute value since transaction_amount is negative for purchases. Assign 0 for days with no purchases.
-    d.year,
-	SUM(ABS(tf.transaction_amount)) 
+	t.transaction_type_description,
+    	c.category_description,
+    	tf.transaction_description,
+    	COALESCE(ABS(tf.transaction_amount), 0) AS transaction_total,		-- Take absolute value since transaction_amount is negative for purchases. Assign 0 for days with no purchases.
+    	d.year,
+	SUM(ABS(tf.transaction_amount)) 					-- Sum total spent by year up to the current date
 		OVER(
 			PARTITION BY d.year 
-			ORDER BY short_date
+			ORDER BY short_date, transaction_id
 		) AS running_yearly_spend
-FROM date AS d
-LEFT JOIN transaction_facts AS tf
+FROM date AS d									-- Select from date column to keep all dates, not just ones with purchases
+LEFT JOIN transaction_facts AS tf						-- Use left join to keep dates mentioned above
 	ON d.short_date = tf.short_date
+        AND tf.transaction_type_id IN (1, 2)
 LEFT JOIN transaction_type AS t 
 	USING (transaction_type_id)
 LEFT JOIN category AS c 
 	USING (category_id)
 WHERE d.short_date <= CURDATE()
-	AND (t.transaction_type_id IN (1, 2) OR t.transaction_type_id IS NULL)
-	AND year != '2022'
+-- 	AND (t.transaction_type_id IN (1, 2) OR t.transaction_type_id IS NULL)						-- Since a left join was used, we must filter out transactions that
+	AND year != '2022'				-- Not interested in 2022					-- are not purchases, while keeping days without purchases 
 ORDER BY short_date;
--- ------------------------------------------------------------------------------------------------------------------------------------------------------
-# View created to run visual on Power BI dashboard. Calculates running total spent for each category (daily level of granularity) 
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+-- View created to run visual on Power BI dashboard. Calculates running total spent for each category (daily level of granularity) 
 CREATE VIEW daily_category_balance AS
 WITH date_category AS (
 	SELECT 
@@ -96,7 +93,7 @@ WITH date_category AS (
 		SELECT category_id
 		FROM category
 	) AS c
-	WHERE EXTRACT(year FROM d.short_date) != '2022'
+	WHERE d.year != '2022'
 		AND d.short_date <= CURDATE()
 	ORDER BY short_date, category_id
 )
@@ -119,14 +116,14 @@ WHERE EXTRACT(year FROM dc.short_date) != '2022'
 	AND (t.transaction_type_id IN (1, 2) OR t.transaction_type_id IS NULL)
 GROUP BY dc.short_date, dc.category_id
 ORDER BY dc.short_date, dc.category_id;
--- ------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Calculates balances of all accounts at the end of each month
 CREATE VIEW monthly_account_balances AS
 SELECT
-	CONCAT(year, '-' , LPAD(month_number, 2, '0')) AS end_date_period,
+	CONCAT(year, '-' , LPAD(month_number, 2, '0')) AS end_date_period,					-- Combine month and year to get date period
 	tf.account_id,
     account_type,
-    SUM(SUM(transaction_amount)) 
+    SUM(SUM(transaction_amount)) 										-- Running total represents account balances
 		OVER(
 			PARTITION BY tf.account_id 
             ORDER BY CONCAT(year, '-' , LPAD(month_number, 2, '0'))
